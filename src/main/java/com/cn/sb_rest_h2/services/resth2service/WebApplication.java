365c1e4f64a402e137127cfd89487a999e92c30f
Weidlich.Robert <weidlich.robert@ensco.com>
2019-04-27 14:00:13 -0400

    Compiles, runs in IntelliJ, responds to "curl localhost:8011", but h2-console does not work.  Setting aside for now.

---
+++ src/main/java/com/cn/sb_rest_h2/services/resth2service/WebApplication.java
@@ -0,0 +1,12 @@
+package com.cn.sb_rest_h2.services.resth2service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
